739d6b35cf4b0a21922652a506f87a452c24268b
agutierrezmonroy <agutierrezmonroy@csumb.edu>
2025-11-05 10:16:53 -0800

    Set up getInformationFromDisplay

--- app/src/main/java/com/example/hw04_gymlog_v300/MainActivity.java
+++ app/src/main/java/com/example/hw04_gymlog_v300/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.hw04_gymlog_v300;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -14,7 +15,11 @@ import com.example.hw04_gymlog_v300.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "DAC_GYMLOG";
     ActivityMainBinding binding;
+    String exercise = "";
+    double weight = 0.0;
+    int reps = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +30,29 @@ public class MainActivity extends AppCompatActivity {
         binding.logButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Grean", Toast.LENGTH_SHORT).show();
+                getInformationFromDisplay();
             }
         });
+
+    }
+
+
+    private void getInformationFromDisplay(){
+        exercise = binding.exerciseInputEditText.getText().toString();
+
+        try {
+            weight = Double.parseDouble(binding.weightInputEditText.getText().toString());
+        } catch (NumberFormatException e) {
+            Log.d(TAG, "Error reading value from Weight edit text.");
+        }
+
+        try {
+            reps = Integer.parseInt(binding.repsInputEditText.getText().toString());
+        } catch (NumberFormatException e) {
+            Log.d(TAG, "Error reading value from Reps edit text.");
+        }
+
+
 
     }
 }
